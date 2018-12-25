044283399ada05d45889f8c89ac818be08046301
Caleb Fung <caleb.t.fung@gmail.com>
2018-12-24 23:42:03 -0600

    add /events endpoint

--- src/slack_slack_app/core.clj
+++ src/slack_slack_app/core.clj
@@ -1,12 +1,22 @@
 (ns slack-slack-app.core
-    (:require [yada.yada :refer [listener resource as-resource]]))
+    (:require [compojure.core :refer :all]
+              [compojure.route :as route]
+              [compojure.handler :as handler]
+              [ring.middleware.json :as middleware]
+              [org.httpkit.server :refer [run-server]]))
 
-(def svr
-  (listener
-   ["/"
-    [
-     ["hello" (as-resource "Hello World!")]
-     ["test" (resource {:produces "text/plain"
-                        :response "This is a test!"})]
-     [true (as-resource nil)]]]
-   {:port 9431}))
+(defroutes app-routes
+  (POST "/events" request 
+    (let [challenge (get-in request [:body :challenge])]
+        {:status 200
+         :body {:challenge challenge}}))
+  (route/not-found "<h1>Page not found</h1>"))
+
+(def app
+  (-> (handler/site app-routes)
+      (middleware/wrap-json-body {:keywords? true})
+      middleware/wrap-json-response))
+
+(defn -main []
+  (run-server app {:port 9431})
+  (println "Server started on port 9431"))
